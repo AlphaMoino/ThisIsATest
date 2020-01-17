f8b39e44dbb248cbfb17a74aecbfda4bf375b130
RViereck <rene@karsten-viereck.de>
2020-01-17 20:18:37 +0100

    Codeexchange
    1234

---
+++ Assets/Scripts/QuaternionTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuaternionTest : MonoBehaviour
+{
+    GameObject capsule;
+    Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        capsule = GameObject.Find("Capsule");
+        target = capsule.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 lookVector = target.position - gameObject.transform.position;
+        gameObject.transform.rotation = Quaternion.LookRotation(lookVector);
+    }
+}
---
+++ Assets/Scripts/Quinternion2.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quinternion2 : MonoBehaviour
+{
+    GameObject cube;
+    Transform target;
+    Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cube = GameObject.Find("Cube");
+        target = cube.GetComponent<Transform>();
+        offset = target.position - gameObject.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        float desiredAngle = target.transform.eulerAngles.y;
+        Quaternion rotation = Quaternion.Euler(0, desiredAngle, 0);
+        transform.position = target.transform.position - (rotation * offset);
+        transform.LookAt(target.transform);
+
+    }
+}
